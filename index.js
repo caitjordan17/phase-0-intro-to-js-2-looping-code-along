b067dc5bcf69a2b16ba342d6a9b0087984c94f7e
Caitlin Jordan <jordan.cait17@gmail.com>
2023-01-28 12:27:36 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solutions in this file
+/*const name = [];
+
+function writeCards(name, occasion){
+    for(let i = 0; i < name.length; i++){
+        return `Thank you, ${name[i]}, for the wonderful ${occasion} gift!`
+    }
+}
+writeCards(name,occasion);
+
+const gifts = ['ball', 'bone', 'bear'];
+
+function wrapGifts(gifts){
+    for (let i=0; i < gifts.length; i++){
+        console.log(`Wrapped ${gifts[i]}!`)
+        debugger;
+    }
+    return gifts;
+}
+wrapGifts(gifts);*/
+
+function writeCards(name,event){
+    let messages = []
+    for(let i=0; i < name.length; i++){
+        messages.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return messages;
+}
+function countDown(i) {
+    while (i >= 0) {
+      console.log(parseFloat(`${i}`));
+      i--;
+    }
+  }
+  
+  countDown();
